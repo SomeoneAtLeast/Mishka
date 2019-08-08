87862275bfdb1102a5c070d869c9454a88a7f00c
SomeoneAtLeast <winecooler@yandex.ru>
2019-08-08 23:53:55 +0300

    Контентная часть + анимация меню

--- js/script.js
+++ js/script.js
@@ -2,16 +2,20 @@ var menu_open_btn = document.querySelector(".main-navigation__btn");
 var main_navigation = document.querySelector(".main-navigation");
 var main_navigation__list_item = document.querySelectorAll(".main-navigation__list-item");
 var main_navigation__list_item_logo = document.querySelector(".main-navigation__list-item--logo");
-
+var main_content = document.querySelector(".main-content");
 
 main_navigation__list_item.forEach(n => n.classList.remove("nojs"))
 
 menu_open_btn.addEventListener("click", function (evt) {
 	if (!menu_open_btn.classList.contains("main-navigation__btn--open-menu")) {
 		evt.preventDefault();
-		menu_open_btn.classList.add("main-navigation__btn--open-menu");
-		main_navigation__list_item.forEach(n => n.classList.add("open-menu"));
-		main_navigation__list_item_logo.classList.remove("open-menu");
+    	menu_open_btn.classList.add("main-navigation__btn--open-menu");
+    	main_content.classList.add("main-content-down");
+		setTimeout(function(){
+			main_navigation__list_item.forEach(n => n.classList.add("open-menu"));
+			main_navigation__list_item_logo.classList.remove("open-menu");
+			main_content.classList.remove("main-content-down");
+			}, 300);
 	} else if (menu_open_btn.classList.contains("main-navigation__btn--open-menu")
 		 ) {
 			menu_open_btn.classList.remove("main-navigation__btn--open-menu");
@@ -19,7 +23,12 @@ menu_open_btn.addEventListener("click", function (evt) {
 			main_navigation__list_item.forEach(n => n.classList.add("close-menu"));
 			main_navigation__list_item_logo.classList.remove("close-menu");
 			setTimeout(function(){
-    			main_navigation__list_item.forEach(n => n.classList.remove("close-menu"));
+				main_content.classList.remove("main-content-down");
+				main_content.classList.add("main-content-up");
 			}, 300);
+			setTimeout(function(){
+    			main_navigation__list_item.forEach(n => n.classList.remove("close-menu"));
+    			main_content.classList.remove("main-content-up");
+			}, 600);
 	}
 }); 
